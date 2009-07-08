5d43b434e3636ab9c23b29a4caebe2bfd4fb340a
Jeff Reinecke <jeff@paploo.net>
2009-07-08 11:28:42 -0700

    Random: Done for now.

--- lib/tools/random.rb
+++ lib/tools/random.rb
@@ -1,20 +1,5 @@
 module Random
   
-  def self.mag_float(magnitude=1)
-    n = nil
-    if(magnitude.kind_of?(Numeric))
-      n = magnitude.to_i
-    elsif(magnitude.kind_of?(Array))
-      n = magnitude.sample.to_i
-    elsif(magnitude.kind_of?(Range))
-      n = magnitude.to_a.sample.to_i
-    else
-      n = magnitude.to_i
-    end
-    
-    return rand()*10**n
-  end
-  
   def self.float(*args)
     case args.length
     when 0
@@ -33,15 +18,4 @@ module Random
     return rand()*(max.to_f-min.to_f) + min.to_f
   end
   
-end
-
-if $0==__FILE__
-  puts Random.mag_float
-  puts Random.mag_float(5)
-  puts Random.mag_float(-1)
-  puts Random.mag_float((1..3))
-  
-  puts Random.float()
-  puts Random.float(20)
-  puts Random.float(5,6)
 end
--- spec/random_spec.rb
+++ spec/random_spec.rb
@@ -3,81 +3,44 @@ require File.join(File.dirname(__FILE__), 'spec_helper')
 describe Random do
   
   it 'should make random floats in (0..1)' do
-    n = 1000
-    min = 0.0
-    max = 1.0
-    bin_count = 10
-    bins = Array.new(bin_count) {0}
-    
-    # Get the data, and check the ranges
-    n.times do
-      x = Random.float
-      x.should <= max
-      x.should >= min
-      index = (x*10).floor
-      bins[index] += 1
-    end
-    
-    # Now check bin counts
-    puts "#{__LINE__}: #{bins.inspect}"
-    expectation = n.to_f/bin_count.to_f
-    error = Math.sqrt(n.to_f)
-    bins.each_with_index do |count, index|
-      count.should >= expectation-error
-      count.should <= expectation+error
-    end
+    float_test(1000, 0.0, 1.0, 10) {Random.float()}
   end
   
   it 'should make random floats with a max value' do
-    n = 1000
-    min = 0.0
-    max = 10.0
-    bin_count = 10
-    bins = Array.new(bin_count) {0}
-    
-    # Get the data, and check the ranges
-    n.times do
-      x = Random.float(max)
-      x.should <= max
-      x.should >= min
-      index = x.floor
-      bins[index] += 1
-    end
-    
-    # Now check bin counts
-    puts "#{__LINE__}: #{bins.inspect}"
-    expectation = n.to_f/bin_count.to_f
-    error = Math.sqrt(n.to_f)
-    bins.each_with_index do |count, index|
-      count.should >= expectation-error
-      count.should <= expectation+error
-    end
+    float_test(1000, 0.0, 10.0, 10) {|min,max| Random.float(max)}
   end
   
   it 'should make random floats with a min and max' do
-    n = 1000
-    min = 10.0
-    max = 20.0
-    bin_count = 10
-    bins = Array.new(bin_count) {0}
+    float_test(1000, 10.0, 20.0, 10) {|min,max| Random.float(min, max)}
+  end
+  
+  def float_test(n, min, max, bins, &test_block)
+    histogram = Array.new(bins.to_i) {0}
     
-    # Get the data, and check the ranges
+    # Get the data and check ranges
     n.times do
-      x = Random.float(min, max)
+      x = test_block.call(min, max)
+      
       x.should <= max
       x.should >= min
-      index = (x-min).floor
-      bins[index] += 1
+      
+      bin_width = (max-min)/bins.to_f
+      index = ((x-min)/bin_width).floor
+      index.should < histogram.length
+      index.should >= 0
+      histogram[index] += 1
     end
     
-    # Now check bin counts
-    puts "#{__LINE__}: #{bins.inspect}"
-    expectation = n.to_f/bin_count.to_f
-    error = Math.sqrt(n.to_f)
-    bins.each_with_index do |count, index|
+    # Now check the bin counts are within 1 std dev.
+    puts histogram.inspect
+    expectation = n.to_f/bins.to_f
+    error = Math.sqrt(n.to_f) # 1 std dev
+    histogram.each do |count|
       count.should >= expectation-error
       count.should <= expectation+error
     end
+    
+    return histogram
   end
   
 end
