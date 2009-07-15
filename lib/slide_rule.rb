9479251a0848abf46c96f47143e3bf5db4a56686
Jeff Reinecke <jeff@paploo.net>
2009-07-14 18:59:15 -0700

    Added some slide rules to the rule library.

--- lib/slide_rule.rb
+++ lib/slide_rule.rb
@@ -10,6 +10,9 @@ class SlideRule
   
   
   MODELS = {
-    :pickett_N4ES => self.new('Pickett', 'N4-ES', 10.00)
+    :pickett_N4ES => self.new('Pickett', 'N4-ES', 9.84)
+    :pickett_N500ES => self.new('Pickett', 'N-500-ES', 9.84)
+    :picket_N600ES => self.new('Pickett', 'N600-ES', 5.92)
+    :post_1460 => self.new('Post', '1460', 9.86)
   }
 end
