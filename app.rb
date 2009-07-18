e3c3048a1c5a0f5b3dfd4547db12bc87e1b0a944
Jeff Reinecke <jeff@paploo.net>
2009-07-17 22:18:08 -0700

    App: Give a warning if the ruby version is not valid.

--- app.rb
+++ app.rb
@@ -1,3 +1,5 @@
+STDERR.puts "#{'*'*72}\nThis application is designed for Ruby 1.9.1,\nYou may experience bugs and crashes running ruby #{RUBY_VERSION}.\n#{'*'*72}" if RUBY_VERSION<'1.9.1'
+
 require File.join(File.dirname(__FILE__), 'config', 'preconfig')
 
 $LOAD_PATH.unshift File.join(File.dirname(__FILE__), 'lib')
