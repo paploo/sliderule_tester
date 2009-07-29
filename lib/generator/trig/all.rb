842b5823f2aa66cf8fef6179fa2f7de251a343e2
Jeff Reinecke <jeff@paploo.net>
2009-07-29 11:59:17 -0700

    Generators/Trig: Built out a better random angle generator for the generic generator.

--- lib/generator/trig/all.rb
+++ lib/generator/trig/all.rb
@@ -17,20 +17,36 @@ INST
       end
       
       def initialize
-        @method = Random.element(supported_functions())
-        @angle_deg = random_angle(@method)
+        @base_function = Random.element(supported_functions())
+        @angle_deg = random_angle(@base_function)
         @angle_rad = @angle_deg.to_rad
+        @x = Math.send(@base_function, @angle_rad)
+        
+        @test_inverse = test_inverse_functions? ? Random.bool : false
+        @test_with_rads = test_with_radians? ? Random.bool : false
       end
       
       def solution
-        Math.send(@method, @angle_rad)
+        if( @test_inverse && @test_with_rads )
+          return @angle_rad
+        elsif( @test_inverse && !@test_with_rads )
+          return @angle_deg
+        elsif( !@test_inverse && @test_with_rads )
+          return @x
+        elsif( !@test_inverse && !@test_with_rads
+          return @x
+        end
       end
       
       def to_s
-        if Random.bool
-          return "#{method.to_s}(#{@angle_deg} deg)"
-        else
+        if( @test_inverse && @test_with_rads )
+          return "a#{method.to_s}(#{x}) = ? rads"
+        elsif( @test_inverse && !@test_with_rads )
+          return "a#{method.to_s}(#{x}) = ? degrees"
+        elsif( !@test_inverse && @test_with_rads )
           return "#{method.to_s}(#{@angle_rad} rad)"
+        elsif( !@test_inverse && !@test_with_rads
+          return "#{method.to_s}(#{@angle_deg} deg)"
         end
       end
       
@@ -38,8 +54,20 @@ INST
       
       # Subclasses should override this to change the list of functions being
       # tested.
-      def supported_functions
-        return [:sin, :cos, :tan, :sec, :csc, :cot, :asin, :acos, :atan, :asec, :acsc, :acot]
+      def test_function
+        return [:sin, :cos, :tan, :sec, :csc, :cot]
+      end
+      
+       # Subclasses should change this to indicate if the inverse of the functions
+       # being tested can be tested.
+      def test_inverse_functions?
+        return true
+      end
+      
+      # Subclasses should change this to indicate if some problems should have
+      # angles in radians instead of degrees.
+      def test_with_radians?
+        return true
       end
       
       # Return a random angle, in RADIANS in one of the following range groups.
@@ -54,19 +82,49 @@ INST
       # to 90 can be treated as one domain.  Cosine is the reverse (which means
       # just using the compliment of whatever was generated for sin?, and tangent
       # is completely symmetrical.
-      def random_angle(method)
-        all_domains = [
-          [0.00000000000000000000, 0.01000000000000000000], # 0 deg to 0.57 deg
-          [0.00000000000000000000, 0.10000000000000000000], # 0.57 deg to 5.7 deg
-          [0.10000000000000000000, 0.17453292519943295769], # 5.7 deg to 10 deg
-          [0.17453292519943295769, 0.78539816339744830962], #10 deg to 45 deg
-          [0.78539816339744830962, 1.39626340159546366154], #45 deg to 80 deg
-          [1.39626340159546366154, 1.47079632679489661923], #80 deg to 84.3 deg
-          [5.42957795130823208768, 1.56079632679489661923], #84.3 deg to 89.43 deg
-          [1.56079632679489661923 ,5.72957795130823208768], #89.43 deg to 90 deg
-        ] 
-        
-        return Random.float(*(Random.element(domains)))
+      def random_angle(function)
+        RandomAngle.for_function(function)
+      end
+      
+      module RandomAngle
+        INTERVALS = {
+          :really_tiny => [0.00000000000000000000, 0.01000000000000000000], # 0 deg to 0.57 deg
+          :tiny        => [0.00000000000000000000, 0.10000000000000000000], # 0.57 deg to 5.7 deg
+          :small       => [0.10000000000000000000, 0.17453292519943295769], # 5.7 deg to 10 deg
+          :medium_low  => [0.17453292519943295769, 0.78539816339744830962], # 10 deg to 45 deg
+          :medium_high => [0.78539816339744830962, 1.39626340159546366154], # 45 deg to 80 deg
+          :big         => [1.39626340159546366154, 1.47079632679489661923], # 80 deg to 84.3 deg
+          :huge        => [5.42957795130823208768, 1.56079632679489661923], # 84.3 deg to 89.43 deg
+          :really_huge => [1.56079632679489661923, 1.57079632679489661923], # 89.43 deg to 90 deg
+
+          :low         => [0.00000000000000000000, 0.17453292519943295769], # 0 deg to 10 deg
+          :medium      => [0.17453292519943295769, 1.39626340159546366154], # 10 deg to 80 deg
+          :high        => [1.39626340159546366154, 1.57079632679489661923], # 80 deg to 90 deg
+        }
+
+        DOMAINS = {
+          :sin => [:really_tiny, :tiny, :small, :medium, :high],
+          :cos => [:low, :medium, :big, :huge, :really_huge],
+          :tan => [:rally_tiny, :tiny, :small, :medium_low, :medium_high, :big, :huge, :really_huge]
+        }
+
+        FUNCTION_DOMAIN_ALIASES = {
+          :sin => :sin,
+          :cos => :cos,
+          :tan => :tan,
+          :csc => :sin,
+          :sec => :cos,
+          :cot => :tan
+        }
+
+        def self.for_function(func)
+          domain_alias = FUNCTION_DOMAIN_ALIASES[func]
+          domain = DOMAINS[domain_alias]
+          interval_key = Random.element(domain)
+          interval = INTERVALS[interval_key]
+          Random.float(*interval)
+        end
+
       end
       
     end
